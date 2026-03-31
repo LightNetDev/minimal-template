6a9a4cfb39959edce3fb93804b8a548781eb9d7f
Simon <lgtnt@protonmail.com>
2026-03-31 11:21:06 +0200

    Update to LightNet v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,38 +2,43 @@
 import { defineConfig } from "astro/config";
 import lightnet from "lightnet";
 
-/**
- * @type {import('lightnet').Language[]}
- */
-const languages = [
-  {
-    code: "en",
-    label: "English",
-    isDefaultSiteLanguage: true,
-  },
-];
-
 export default defineConfig({
   // TODO: Set the site's domain.
   site: "https://your-site-domain.com",
   integrations: [
     lightnet({
-      title: "x.site.title",
-      languages,
+      title: {
+        en: "Media Library",
+      },
+      languages: [
+        {
+          code: "en",
+          label: {
+            en: "English",
+          },
+          isDefaultSiteLanguage: true,
+        },
+      ],
       favicon: [{ href: "favicon.svg" }],
       credits: true,
       mainMenu: [
         {
           href: "/",
-          label: "ln.home.title",
+          label: {
+            en: "Home",
+          },
         },
         {
           href: "/media",
-          label: "ln.search.title",
+          label: {
+            en: "Media",
+          },
         },
         {
           href: "/about",
-          label: "x.navigation.about",
+          label: {
+            en: "About",
+          },
         },
       ],
     }),
